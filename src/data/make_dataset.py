8d22da80804a981fb6dd7f82a1bf599065cbc8b3
Kegmar <max.dorozko@gmail.com>
2023-05-29 22:25:58 +0300

    initial commit

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,31 +1,4 @@
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 
-df = pd.read_csv('../../data/raw/bike/daily.csv')
-
-print(df)
-
-# Select features and target variable
-features = ['season', 'yr', 'mnth', 'holiday', 'weekday', 'workingday', 'weathersit', 'temp', 'atemp', 'hum', 'windspeed']
-target = 'rentals'
-
-# Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(df[features], df[target], test_size=0.2, random_state=42)
-
-# Initialize the linear regression model
-model = LinearRegression()
-
-# Train the model
-model.fit(X_train, y_train)
-
-# Make predictions on the test set
-y_pred = model.predict(X_test)
-
-# Evaluate the model
-mse = mean_squared_error(y_test, y_pred)
-print('Mean Squared Error:', mse)
 
 
 
